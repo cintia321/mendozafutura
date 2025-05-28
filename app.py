ffc0319ef72b99984b671922c40d9e1e0e69f847
cintia321 <giselachoque2005@gmail.com>
2025-05-28 20:34:40 +0000

    29-05-25

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route('/')
 def home(): 
- return render_template('index.html')
+    return render_template('index.html')
+
+@app.route ("/proyectos")
+def proyectos():
+    return render_template('proyectos.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+ 
